d160be6ba46704671a6d06e1137cb140edc49e8f
jlucero805 <jlucero805@gmail.com>
2023-11-28 17:56:06 -0800

    Install React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
 function App() {
-  return (
-    <>
-       <div className="rounded-lg px-3 py-2 bg-red-100"> 
-        Hello, world!
-       </div> 
-    </>
-  )
+    const { data: helloWorld } = useQuery({
+        queryKey: ["hello-world-query"],
+        queryFn: () => "Hello, world!",
+    });
+
+    return (
+        <>
+            <div className="rounded-lg px-3 py-2 bg-red-100"> 
+                { helloWorld }
+            </div> 
+        </>
+    )
 }
 
 export default App
