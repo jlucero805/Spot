2b4ad8abb1b883234da380ddfae39de31965874a
jlucero805 <jlucero805@gmail.com>
2023-11-28 17:58:51 -0800

    Install React Router

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,21 @@ import App from './App.tsx'
 import './index.css'
 import '@tanstack/react-query'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    }
+]);
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
      <QueryClientProvider client={queryClient}> 
-        <App />
+        <RouterProvider router={router} />
      </QueryClientProvider> 
   </React.StrictMode>,
 )
